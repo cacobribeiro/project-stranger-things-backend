38051cc53b590ae0c989967a30b52e60c6121f1e
Caio Ribeiro <cacoribeiro@gmail.com>
2020-11-05 17:46:56 -0300

    Fix PORT .env ?

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
-});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
